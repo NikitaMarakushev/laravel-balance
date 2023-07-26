cde8cfeeed9bb0e131238cbe62369edb27bea3d6
NikitaMarakushev <nikitamarakushev@yandex.ru>
2023-07-26 16:44:32 +0300

    BalanceCalculatorValidatorTest test

---
+++ tests/Unit/Domain/BalanceCalculatorValidatorTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Domain;
+use PhpMyAdmin\SqlParser\Tests\TestCase;
+use App\Domain\Validator\BalanceCalculatorValidator;
+
+class BalanceCalculatorValidatorTest extends TestCase
+{
+    public function testValidate(): void
+    {
+        //@todo нужен метод, который тестирует валидацию
+    }
+}
